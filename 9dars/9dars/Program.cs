93b9c18ff8c17905941dde2651f5b5d2d2039958
rasullbe <rasulbek112208@gmail.com>
2026-02-28 17:37:18 +0500

    list

---
+++ 9dars/9dars/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Linq;
+
+namespace _9dars;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //12 пример
+        ArrayList arrayList = new ArrayList();
+        arrayList.Add(1);
+        arrayList.Add(2);
+        arrayList.Add(7);
+        arrayList.Add(4);
+        arrayList.Add(5);
+        var max = arrayList.Cast<int>().Max();
+        Console.WriteLine(max);
+
+        //11 пример 
+        ArrayList arrayList1 = new ArrayList();
+        arrayList1.Add(1);
+        arrayList1.Add(2);
+        arrayList1.Add(7);
+        arrayList1.Add(4);
+        arrayList1.Add(5);
+        var summ = arrayList1.Cast<int>().Sum();
+        Console.WriteLine(summ);
+    }
+}
