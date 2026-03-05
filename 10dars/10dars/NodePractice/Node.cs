5d2c44cbdf0b643acbd5c3d2c0e802c0412deed1
rasullbe <rasulbek112208@gmail.com>
2026-03-05 22:21:05 +0500

    dars

---
+++ 10dars/10dars/NodePractice/Node.cs
@@ -0,0 +1,13 @@
+﻿namespace _10dars.NodePractice;
+
+public class Node
+{
+    public int Value;
+    public Node Next;
+
+    public Node(int value = 0)
+    {
+        Value = value;
+        Next = null;
+    }
+}
---
+++ 10dars/10dars/NodePractice/NodeService.cs
@@ -0,0 +1,20 @@
+﻿namespace _10dars.NodePractice;
+
+public class NodeService
+{
+    public static Node CreateNode(int length)
+    {
+        Random random = new Random();
+        Node headNode = new Node(random.Next(2, 9));
+        Node currentNode = headNode;
+
+        for (int i = 1; i < length; i++)
+        {
+            Node newNode = new Node(random.Next(2, 9));
+            currentNode.Next = newNode;
+            currentNode = currentNode.Next;
+        }
+
+        return headNode;
+    }
+}
